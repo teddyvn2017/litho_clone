83de03099da7601c9f5cff4d10f033e3b7118ed3
teddyvn2017 <ware4u@gmail.com>
2025-03-27 00:54:32 +0700

    updating hide submenu when mouse click out

--- src/asset/js/submenu.js
+++ src/asset/js/submenu.js
@@ -27,3 +27,15 @@ productsMenu.addEventListener("click", function(e) {
         dropdownIcon.classList.toggle("rotate-180");
     }
 });
+
+// Ẩn submenu khi click ra ngoài
+document.addEventListener("click", function (e) {
+    // Kiểm tra nếu click **không nằm bên trong #products-menu**
+    if (!productsMenu.contains(e.target)) {
+      submenu.classList.add("opacity-0", "invisible");
+      submenu.classList.remove("opacity-100", "visible");
+  
+      // Reset lại mũi tên về trạng thái cũ
+      dropdownIcon.classList.remove("rotate-180");
+    }
+  });
